52e53d5599d40a66e9bbdbed430e7321631acfa4
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 14:54:36 -0500

    aaaa

--- manejadordocumentos/manejadordocumentos/views.py
+++ manejadordocumentos/manejadordocumentos/views.py
@@ -20,9 +20,9 @@ from django.shortcuts import render, redirect
 
 from django.core.mail import send_mail
 from django.conf import settings
-from ..documentos.decorators import token_required
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from manejadordocumentos.documentos.decorators import token_required
 
 
 #Llave publica
