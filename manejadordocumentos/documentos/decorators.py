78650c4fdf4b6ed98b35f8945040557fd0d4004a
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 16:14:20 -0500

    a ver

--- manejadordocumentos/documentos/decorators.py
+++ manejadordocumentos/documentos/decorators.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 def token_required(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         if not request.session["user_token"]:
-            return HttpResponse(status=404, content={"msg": "Token no está en sesión"})
+            return redirect("http://35.184.109.166:8080/loginPage")
         else:
             print("listo para autenticar token")
             response = requests.get("http://35.190.51.156:8082/user/is_authenticated?token=" + request.session["user_token"])
