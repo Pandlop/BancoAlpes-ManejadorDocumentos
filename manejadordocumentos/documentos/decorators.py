45a182bae6d7ee92f5bd527f323e8ac151426759
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 15:59:22 -0500

    solucion sus

--- manejadordocumentos/documentos/decorators.py
+++ manejadordocumentos/documentos/decorators.py
@@ -13,8 +13,7 @@ def token_required(view_func):
             print("listo para autenticar token")
             response = requests.get("http://35.190.51.156:8082/user/is_authenticated?token=" + request.session["user_token"])
             
-            
-            if "error" in response.json():
+            if "error" in response.text:
                 return redirect("http://35.184.109.166:8080/loginPage")
         return view_func(request, *args, **kwargs)
     return _wrapped_view_func
