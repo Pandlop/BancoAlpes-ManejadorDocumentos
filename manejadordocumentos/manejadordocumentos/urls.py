8460e1e64a20a98dab51e4b078b774934dc8bde3
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 03:40:39 -0500

    mmm

--- manejadordocumentos/manejadordocumentos/urls.py
+++ manejadordocumentos/manejadordocumentos/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('documentos/', include('documentos.urls')),
-    path('/health-check/', views.healthCheck),
+    path('health-check/', views.healthCheck),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
