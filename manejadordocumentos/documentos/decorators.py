1127018aabf7dfe3c8c727baca81fbde81825945
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 15:50:03 -0500

    get header

--- manejadordocumentos/documentos/decorators.py
+++ manejadordocumentos/documentos/decorators.py
@@ -11,7 +11,7 @@ def token_required(view_func):
             return HttpResponse(status=404, content={"msg": "Token no está en sesión"})
         else:
             print("listo para autenticar token")
-            response = requests.get("http://35.190.51.156:8082/user/is_authenticated", headers=request.session["user_token"])
+            response = requests.get("http://35.190.51.156:8082/user/is_authenticated?token=" + request.session["user_token"])
             print(response.text)
         return view_func(request, *args, **kwargs)
     return _wrapped_view_func
